bee4c1a2610ff2d5d998691eaeb5f9d901fc35aa
Elmar Sonnenschein <eso@esoco.de>
2016-05-13 21:30:18 +0200

    FRAMEWORK-180 (Bug): Integrate the GWT Material Design project with the framework

---
+++ src/main/java/de/esoco/ewt/impl/gwt/material/layout/MaterialGridLayout.java
@@ -0,0 +1,78 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'gewt-material' project.
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+package de.esoco.ewt.impl.gwt.material.layout;
+
+import gwt.material.design.client.base.MaterialWidget;
+import gwt.material.design.client.ui.MaterialColumn;
+import gwt.material.design.client.ui.MaterialRow;
+
+import de.esoco.ewt.component.Container;
+import de.esoco.ewt.layout.GenericLayout;
+import de.esoco.ewt.style.StyleData;
+
+import de.esoco.lib.property.UserInterfaceProperties.Layout;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+
+/********************************************************************
+ * A GwtMaterial layout implementation for elements in a responsive grid.
+ *
+ * @author eso
+ */
+final class MaterialGridLayout extends GenericLayout
+{
+	//~ Instance fields --------------------------------------------------------
+
+	private Layout eLayout;
+
+	//~ Constructors -----------------------------------------------------------
+
+	/***************************************
+	 * Creates a new instance.
+	 *
+	 * @param eLayout The layout
+	 */
+	public MaterialGridLayout(Layout eLayout)
+	{
+		this.eLayout = eLayout;
+	}
+
+	//~ Methods ----------------------------------------------------------------
+
+	/***************************************
+	 * {@inheritDoc}
+	 */
+	@Override
+	public HasWidgets createLayoutContainer(
+		Container rContainer,
+		StyleData rStyle)
+	{
+		MaterialWidget rContainerWidget;
+
+		if (eLayout == Layout.GRID_ROW)
+		{
+			rContainerWidget = new MaterialRow();
+		}
+		else
+		{
+			rContainerWidget = new MaterialColumn();
+		}
+
+		return rContainerWidget;
+	}
+}
