5062b680170bbea4097b2acde6eb582cb63c6365
Elmar Sonnenschein <eso@esoco.de>
2016-07-05 20:23:07 +0200

    FRAMEWORK-180: Integrate the GWT Material Design project with the framework
    NEW: support for floating elements and MaterialFAB

---
+++ src/main/java/de/esoco/ewt/impl/gwt/material/widget/GewtMaterialAnchorButton.java
@@ -0,0 +1,60 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'gewt-material' project.
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+package de.esoco.ewt.impl.gwt.material.widget;
+
+import gwt.material.design.client.ui.MaterialAnchorButton;
+import gwt.material.design.client.ui.MaterialButton;
+
+import de.esoco.ewt.graphics.Image;
+import de.esoco.ewt.property.ImageAttribute;
+
+import com.google.gwt.user.client.ui.HasText;
+
+
+/********************************************************************
+ * A {@link MaterialButton} subclass that also implements {@link HasText}.
+ *
+ * @author eso
+ */
+public class GewtMaterialAnchorButton extends MaterialAnchorButton
+	implements HasText, ImageAttribute
+{
+	//~ Instance fields --------------------------------------------------------
+
+	private ImageAttributeMixin<GewtMaterialAnchorButton> aImageAttrMixin =
+		new ImageAttributeMixin<>(this);
+
+	//~ Methods ----------------------------------------------------------------
+
+	/***************************************
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Image getImage()
+	{
+		return aImageAttrMixin.getImage();
+	}
+
+	/***************************************
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setImage(Image rImage)
+	{
+		aImageAttrMixin.setImage(rImage);
+	}
+}
---
+++ src/main/java/de/esoco/ewt/impl/gwt/material/widget/InternalMaterialSwitch.java
@@ -0,0 +1,327 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'gewt-material' project.
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+package de.esoco.ewt.impl.gwt.material.widget;
+
+import gwt.material.design.client.base.HasError;
+import gwt.material.design.client.base.MaterialWidget;
+import gwt.material.design.client.base.mixin.ErrorMixin;
+import gwt.material.design.client.constants.InputType;
+import gwt.material.design.client.ui.MaterialInput;
+import gwt.material.design.client.ui.MaterialLabel;
+import gwt.material.design.client.ui.html.Label;
+import gwt.material.design.client.ui.html.Span;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.HasValue;
+
+
+/********************************************************************
+ * Contains a workaround for a material switch event handling bug:
+ * https://github.com/GwtMaterialDesign/gwt-material/issues/389
+ *
+ * @author eso
+ */
+class InternalMaterialSwitch extends MaterialWidget
+	implements HasValue<Boolean>, HasClickHandlers, HasError
+{
+	//~ Instance fields --------------------------------------------------------
+
+	private MaterialInput input    = new MaterialInput();
+	private Span		  span     = new Span();
+	private Label		  label    = new Label();
+	private MaterialLabel lblError = new MaterialLabel();
+	private Span		  onLabel  = new Span();
+	private Span		  offLabel = new Span();
+
+	private final ErrorMixin<InternalMaterialSwitch, MaterialLabel> errorMixin =
+		new ErrorMixin<>(this, lblError, null);
+
+	//~ Constructors -----------------------------------------------------------
+
+	/***************************************
+	 * Creates a switch element
+	 */
+	public InternalMaterialSwitch()
+	{
+		super(Document.get().createDivElement(), "switch");
+		span.setStyleName("lever");
+		input.setType(InputType.CHECKBOX);
+
+		//register click handler here in order to have it at first position
+		// and therefore it will deal with clicks as first and setup the value
+		// right before others get notified.
+		addClickHandler(new ClickHandler()
+			{
+				@Override
+				public void onClick(ClickEvent event)
+				{
+					setValue(!getValue());
+					event.preventDefault();
+					event.stopPropagation();
+				}
+			});
+	}
+
+	//~ Methods ----------------------------------------------------------------
+
+	/***************************************
+	 * @see com.google.gwt.event.dom.client.HasClickHandlers#addClickHandler(com.google.gwt.event.dom.client.ClickHandler)
+	 */
+	@Override
+	public HandlerRegistration addClickHandler(final ClickHandler handler)
+	{
+		return addDomHandler(new ClickHandler()
+			{
+				@Override
+				public void onClick(ClickEvent event)
+				{
+					if (isEnabled())
+					{
+						handler.onClick(event);
+					}
+				}
+			},
+							 ClickEvent.getType());
+	}
+
+	/***************************************
+	 * TODO: DOCUMENT ME!
+	 *
+	 * @param  handler TODO: DOCUMENT ME!
+	 *
+	 * @return TODO: DOCUMENT ME!
+	 */
+	@Override
+	public HandlerRegistration addValueChangeHandler(
+		final ValueChangeHandler<Boolean> handler)
+	{
+		return addHandler(new ValueChangeHandler<Boolean>()
+			{
+				@Override
+				public void onValueChange(ValueChangeEvent<Boolean> event)
+				{
+					if (isEnabled())
+					{
+						handler.onValueChange(event);
+					}
+				}
+			},
+						  ValueChangeEvent.getType());
+	}
+
+	/***************************************
+	 * @see gwt.material.design.client.base.HasError#clearErrorOrSuccess()
+	 */
+	@Override
+	public void clearErrorOrSuccess()
+	{
+		errorMixin.clearErrorOrSuccess();
+	}
+
+	/***************************************
+	 * TODO: DOCUMENT ME!
+	 *
+	 * @return the input
+	 */
+	public MaterialInput getInput()
+	{
+		return input;
+	}
+
+	/***************************************
+	 * TODO: DOCUMENT ME!
+	 *
+	 * @return the label
+	 */
+	@Deprecated
+	public Label getLabel()
+	{
+		return label;
+	}
+
+	/***************************************
+	 * TODO: DOCUMENT ME!
+	 *
+	 * @return the span
+	 */
+	public Span getSpan()
+	{
+		return span;
+	}
+
+	/***************************************
+	 * Gets the value of switch component.
+	 *
+	 * @return TODO: DOCUMENT ME!
+	 */
+	@Override
+	public Boolean getValue()
+	{
+		return input.getElement().hasAttribute("checked");
+	}
+
+	/***************************************
+	 * @see gwt.material.design.client.base.MaterialWidget#setEnabled(boolean)
+	 */
+	@Override
+	public void setEnabled(boolean enabled)
+	{
+		super.setEnabled(enabled);
+		getElement().setAttribute("style",
+								  "background-color:transparent !important");
+		label.getElement()
+			 .setAttribute("style", "background-color:transparent !important");
+		span.setEnabled(enabled);
+		input.setEnabled(enabled);
+	}
+
+	/***************************************
+	 * @see gwt.material.design.client.base.HasError#setError(java.lang.String)
+	 */
+	@Override
+	public void setError(String error)
+	{
+		errorMixin.setError(error);
+	}
+
+	/***************************************
+	 * TODO: DOCUMENT ME!
+	 *
+	 * @param input the input to set
+	 */
+	public void setInput(MaterialInput input)
+	{
+		this.input = input;
+	}
+
+	/***************************************
+	 * TODO: DOCUMENT ME!
+	 *
+	 * @param label the label to set
+	 */
+	@Deprecated
+	public void setLabel(Label label)
+	{
+		this.label = label;
+	}
+
+	/***************************************
+	 * Set the Off State Label of the switch component
+	 *
+	 * @param label TODO: DOCUMENT ME!
+	 */
+	public void setOffLabel(String label)
+	{
+		offLabel.setText(label);
+	}
+
+	/***************************************
+	 * Set the On State Label of the switch component
+	 *
+	 * @param label TODO: DOCUMENT ME!
+	 */
+	public void setOnLabel(String label)
+	{
+		onLabel.setText(label);
+	}
+
+	/***************************************
+	 * TODO: DOCUMENT ME!
+	 *
+	 * @param span the span to set
+	 */
+	public void setSpan(Span span)
+	{
+		this.span = span;
+	}
+
+	/***************************************
+	 * @see gwt.material.design.client.base.HasError#setSuccess(java.lang.String)
+	 */
+	@Override
+	public void setSuccess(String success)
+	{
+		errorMixin.setSuccess(success);
+	}
+
+	/***************************************
+	 * @see com.google.gwt.user.client.ui.HasValue#setValue(java.lang.Boolean)
+	 */
+	@Override
+	public void setValue(Boolean value)
+	{
+		setValue(value, true);
+	}
+
+	/***************************************
+	 * Set the value of switch component.
+	 *
+	 * @param value      TODO: DOCUMENT ME!
+	 * @param fireEvents TODO: DOCUMENT ME!
+	 */
+	@Override
+	public void setValue(Boolean value, boolean fireEvents)
+	{
+		boolean oldValue = getValue();
+
+		if (value)
+		{
+			input.getElement().setAttribute("checked", "true");
+		}
+		else
+		{
+			input.getElement().removeAttribute("checked");
+		}
+
+		if (fireEvents && oldValue != value)
+		{
+			ValueChangeEvent.fire(this, getValue());
+		}
+	}
+
+	/***************************************
+	 * @see gwt.material.design.client.base.MaterialWidget#onLoad()
+	 */
+	@Override
+	protected void onLoad()
+	{
+		super.onLoad();
+
+		if (offLabel.getText() != null && !offLabel.getText().isEmpty())
+		{
+			label.add(offLabel);
+		}
+
+		label.add(input);
+		label.add(span);
+		add(label);
+		add(lblError);
+		lblError.getElement().getStyle().setMarginTop(16, Unit.PX);
+
+		if (onLabel.getText() != null && !onLabel.getText().isEmpty())
+		{
+			label.add(onLabel);
+		}
+	}
+}
