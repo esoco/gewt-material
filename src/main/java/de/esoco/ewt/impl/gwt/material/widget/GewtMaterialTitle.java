f8164627e5b13195f602cfb65015cec62cd3836c
Elmar Sonnenschein <eso@esoco.de>
2016-07-23 18:11:53 +0200

    BF: getTitle()

--- src/main/java/de/esoco/ewt/impl/gwt/material/widget/GewtMaterialTitle.java
+++ src/main/java/de/esoco/ewt/impl/gwt/material/widget/GewtMaterialTitle.java
@@ -1,18 +1,38 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'gewt-material' project.
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 package de.esoco.ewt.impl.gwt.material.widget;
+
+import gwt.material.design.client.ui.MaterialTitle;
 
 import com.google.gwt.user.client.ui.HasText;
 
-import gwt.material.design.client.ui.MaterialTitle;
 
 /********************************************************************
  * A {@link MaterialTitle} subclass that also implements {@link HasText}.
  *
  * @author eso
  */
-public class GewtMaterialTitle extends MaterialTitle
-	implements HasText
+public class GewtMaterialTitle extends MaterialTitle implements HasText
 {
-	//~ Methods ------------------------------------------------------------
+	//~ Instance fields --------------------------------------------------------
+
+	private String sTitleText;
+
+	//~ Methods ----------------------------------------------------------------
 
 	/***************************************
 	 * {@inheritDoc}
@@ -20,7 +40,7 @@ public class GewtMaterialTitle extends MaterialTitle
 	@Override
 	public String getText()
 	{
-		return super.getTitle();
+		return sTitleText;
 	}
 
 	/***************************************
@@ -29,6 +49,8 @@ public class GewtMaterialTitle extends MaterialTitle
 	@Override
 	public void setText(String sText)
 	{
+		sTitleText = sText;
+
 		super.setTitle(sText);
 	}
 
