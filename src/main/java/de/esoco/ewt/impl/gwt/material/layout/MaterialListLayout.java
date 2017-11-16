949266768da3b3524b0c1f523c0ea1006c6408fe
Elmar Sonnenschein <eso@esoco.de>
2017-11-16 17:17:00 +0100

    RF: workaround removed

--- src/main/java/de/esoco/ewt/impl/gwt/material/layout/MaterialListLayout.java
+++ src/main/java/de/esoco/ewt/impl/gwt/material/layout/MaterialListLayout.java
@@ -19,7 +19,6 @@ package de.esoco.ewt.impl.gwt.material.layout;
 import gwt.material.design.client.base.MaterialWidget;
 import gwt.material.design.client.base.helper.StyleHelper;
 import gwt.material.design.client.constants.CollapsibleType;
-import gwt.material.design.client.js.JsMaterialElement;
 import gwt.material.design.client.ui.MaterialCollapsible;
 import gwt.material.design.client.ui.MaterialCollapsibleItem;
 import gwt.material.design.client.ui.MaterialCollection;
@@ -34,7 +33,6 @@ import de.esoco.lib.property.Alignment;
 import de.esoco.lib.property.ListLayoutStyle;
 import de.esoco.lib.property.SingleSelection;
 
-import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -242,25 +240,6 @@ public class MaterialListLayout extends AbstractMaterialLayout
 			}
 
 			collapsible(getElement());
-		}
-
-		/***************************************
-		 * {@inheritDoc}
-		 */
-		@Override
-		protected void collapsible(Element rElement)
-		{
-			super.collapsible(rElement);
-
-			JsMaterialElement.$(rElement)
-							 .add(".no-collapse")
-							 .on("click",
-								 e ->
-				 				{
-				 					e.stopPropagation();
-
-				 					return Boolean.TRUE;
-								 });
 		}
 	}
 }
