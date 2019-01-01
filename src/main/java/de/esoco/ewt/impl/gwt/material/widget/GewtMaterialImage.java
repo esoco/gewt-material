a3f79c89ce4daa5da43ed006d7d73e943615c24c
Elmar Sonnenschein <eso@esoco.de>
2019-01-01 18:47:31 +0100

    Javadoc fixed

--- src/main/java/de/esoco/ewt/impl/gwt/material/widget/GewtMaterialImage.java
+++ src/main/java/de/esoco/ewt/impl/gwt/material/widget/GewtMaterialImage.java
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'gewt-material' project.
-// Copyright 2017 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2019 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -74,8 +74,10 @@ public class GewtMaterialImage extends MaterialImage implements HasText,
 
 	/***************************************
 	 * Overridden to prevent error "this$static_0_g$ is null" on updating the
-	 * label. It occurs because {@link AbstractButton} always removes the text
-	 * span in the {@link AbstractButton#setText(String)} method.
+	 * label. It occurs because {@link
+	 * gwt.material.design.client.base.AbstractButton} always removes the text
+	 * span in the {@link
+	 * gwt.material.design.client.base.AbstractButton#setText(String)} method.
 	 *
 	 * @see HasText#setText(String)
 	 */
